28aa1706e82ff651193cf3976d9709228c164e16
marcarAtendimento <joaoneivanetos@gmail.com>
2021-05-04 09:31:59 -0300

    first commit

---
+++ Controllers/PieController.cs
@@ -0,0 +1,25 @@
+﻿using BethenyPieShop2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethenyPieShop2.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+            
+    }
+}
---
+++ Models/MockPieRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethenyPieShop2.Models
+{
+    public class MockPieRepository: IPieRepository
+    {
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+
+        public IEnumerable<Pie> AllPies =>
+            new List<Pie>
+            {
+                new Pie {PieId = 1, Name="Strawberry Pie", Price=15.95M,ShortDescription="Lorem Ipsum", LongDescription="]"},
+                new Pie {PieId=2, Name="Cheese cake",Price=18.95M,ShortDescription="Lorem Ipsum", LongDescription="]"},
+                new Pie {PieId=3, Name="Rhubarb Pie",Price=15.95M,ShortDescription="Lorem Ipsum", LongDescription="]"},
+                new Pie {PieId=4, Name="Pumpkin pie", Price=12.95M, ShortDescription="lorem Ipsum",LongDescription="]"}
+            };
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public ICategoryRepository CategoryRepository => _categoryRepository;
+
+        public Pie GetPieById(int pieId)
+        {
+            return AllPies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
