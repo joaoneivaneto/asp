6d87a28fdeff37bf60c60627b33583a94a408cda
marcarAtendimento <joaoneivanetos@gmail.com>
2021-05-21 12:21:21 -0300

    nun art carrinho menu suspenso com filtro

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -18,20 +18,41 @@ namespace BethenyPieShop2.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
-            piesListViewModel.CurrentCategory = "cheese cakes";
-            return View(piesListViewModel);
+       // public ViewResult List()
+       // {
+            //PiesListViewModel piesListViewModel = new PiesListViewModel();
+            //piesListViewModel.Pies = _pieRepository.AllPies;
+           // piesListViewModel.CurrentCategory = "cheese cakes";
+           // return View(piesListViewModel);
 
-        }
+       // }
         public IActionResult Details(int id)
         {
             var pie = _pieRepository.GetPieById(id);
             if (pie == null)
                 return NotFound();
             return View(pie);
+        }
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName  == category)?.CategoryName;
+                    
+            }
+            return View(new PiesListViewModel{
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
     }
 }
